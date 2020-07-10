b17722f7c85c9a9549c4c028e7064472c48fd924
G-TRITON <braj85216@gmail.com>
2020-07-10 14:48:17 +0530

    adding modified files

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ let h3 = document.querySelector('h3');
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let body = document.getElementById('body');
+let hex = document.querySelector('#hex');
 
 function colorCh(){
     body.style.background = 'linear-gradient(to right, ' + color1.value + ',' + color2.value + ')';
     h3.textContent = body.style.background + ";";    
 }
 
+function convert(){
+	let rgba1 = color1.value;
+	let rgba2 = color2.value;
+	h3.textContent = 'linear-gradient(to right, ' + rgba1 + ',' + rgba2 + ')';
+}
+
+hex.addEventListener('click', convert);
+
 color1.addEventListener('input', colorCh);
 
 color2.addEventListener('input', colorCh)
-
